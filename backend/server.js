2a6d51888c764b77e05a5fe70c6d6e336798d409
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-10-04 10:46:28 +0100

    \\

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express"
 
-const app = express()
+const app = express();
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("you got 6 notes")
+} )
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
