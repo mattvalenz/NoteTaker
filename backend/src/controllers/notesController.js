783f389e7a4804bd2af0277952206f4765838ebc
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-10-16 21:59:25 +0100

    \\

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,11 +1,23 @@
-export function getAllNotes (req, res)  {
-    res.status(200).json({message: "List of notes"})}
+import Note from "../models/Note.js";
 
-    export function createNote (req, res){
-    res.status(201).json({message: "New note created"})}
-    
-    export function updateNote (req, res) {
-    res.status(200).json({message: "Note updated successfully"})}
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find()
+    res.status(200).json(notes)
+  } catch (error) {
+    console.log("Error in getallNotes controller", error)
+    res.status(500).json({ message: "Error fetching notes" })
+  }
+}
 
-    export function deleteNote (req, res) {
-    res.status(200).json({message: "Note deleted successfully"})}
+export function createNote(req, res) {
+  res.status(201).json({ message: "New note created" });
+}
+
+export function updateNote(req, res) {
+  res.status(200).json({ message: "Note updated successfully" });
+}
+
+export function deleteNote(req, res) {
+  res.status(200).json({ message: "Note deleted successfully" });
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,16 @@
-import express from "express"
-import notesRoutes from "./routes/notesRoutes.js"
-import connectDB from "./config/db.js"
-import dotenv from "dotenv"
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 connectDB();
 
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-
-})
-
-
+app.listen(PORT, () => {
+  console.log("Server is running on PORT: ", PORT);
+});
