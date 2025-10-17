489035c2afcaab6826e68ca5fad4e60e868bda73
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-10-17 16:35:59 +0100

    \\

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,34 +2,44 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes(req, res) {
   try {
-    const notes = await Note.find()
-    res.status(200).json(notes)
+    const notes = await Note.find();
+    res.status(200).json(notes);
   } catch (error) {
-    console.log("Error in getallNotes controller", error)
-    res.status(500).json({ message: "Error fetching notes" })
+    console.log("Error in getallNotes controller", error);
+    res.status(500).json({ message: "Error fetching notes" });
   }
 }
 
 export async function createNote(req, res) {
   try {
-    const {title, content} = req.body;
-    const note = new Note({title, content})
-    const savedNote = await note.save()
-    res.status(201).json(savedNote)
+    const { title, content } = req.body;
+    const note = new Note({ title, content });
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
   } catch (error) {
-     console.log("Error in createNote controller", error)
-    res.status(500).json({message: "Error creating note"})
+    console.log("Error in createNote controller", error);
+    res.status(500).json({ message: "Error creating note" });
   }
 }
 
 export async function updateNote(req, res) {
   try {
-    const {title, content} = req.body
-    await Note.findByIdAndUpdate(req.params.id, {title, content})
-    res.status(200).json({message: "Note updated successfully"})
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        content,
+      },
+      { new: true }
+    );
+
+    if (!updatedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json({ message: "Note updated successfully" });
   } catch (error) {
-    console.log("Error in UpdateNote controller", error)
-    res.status(500).json({message: "Error updating note"})
+    console.log("Error in UpdateNote controller", error);
+    res.status(500).json({ message: "Error updating note" });
   }
 }
 
