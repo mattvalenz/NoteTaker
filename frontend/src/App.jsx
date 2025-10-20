6cdb25b0debe4fef0fa81b6544489c91affa3c3f
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-10-20 10:55:52 +0100

    \\

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage.jsx'
-import CreatePage from './pages/CreatePage.jsx'
-import NoteDetailPage from './pages/NoteDetailPage.jsx'
-import toast from 'react-hot-toast'
-
+import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
-      <h2 className='text-xl'>try again</h2>
-      <button onClick={() => toast.error("congrats")} className='bg-red-500'>Click me</button>
+    <div data-theme="forest">
       <Routes>
-        
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Navbar from '../components/Navbar.jsx'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'><Navbar/></div>
   )
 }
 
