0dd235d30c61787d623e3c5684fe3ce7e6a1f9f1
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-10-20 11:07:52 +0100

    \\

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Navbar from '../components/Navbar.jsx'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar.jsx";
+import RateLimitedUI from "../components/RateLimitedUI.jsx";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
   return (
-    <div className='min-h-screen'><Navbar/></div>
-  )
-}
+    <div className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
