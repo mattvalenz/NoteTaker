aa08f6bf08b493ace3da2ec605f055bb7a8c686f
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-10-21 18:58:25 +0100

    \\

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { ArrowLeftIcon } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router";
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default CreatePage
+  const handleSubmit = () => {};
+
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8 ">
+        <div className="max-w-2xl mx-auto">
+          <Link to="/" className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+            Back to Home
+          </Link>
+
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title text-2xl mb-4">Create a New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+
+                  <input type="text" 
+                  placeholder="Note Title"
+                  className="input input-bordered"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}/>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
