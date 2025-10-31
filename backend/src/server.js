90a300c75cf28115710eb7795ce088e18f6bfb5b
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-10-31 13:00:32 +0000

    prod issues, adding proper middleware syntax

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,47 +1,43 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
-import connectDB from "./config/db.js";
-import dotenv from "dotenv";
-import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import dotenv from "dotenv";
 import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-
-if(process.env.NODE_ENV !== "production"){
-  app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:5173' // adjust this to your frontend's origin
-}))
-app.use(rateLimiter)
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
 
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} at ${req.url}`);
-//   next();
-// });
 
 app.use("/api/notes", notesRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"))
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-  console.log("Server is running on PORT: ", PORT);
+    console.log("Server started on PORT:", PORT);
+  });
 });
-
-})
-
-
